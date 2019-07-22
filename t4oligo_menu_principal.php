e1550b278f8510949d370604372534cfa8170abe
ericknegreter <erickxon-roy@hotmail.com>
2019-07-22 17:54:55 -0500

    mi primer commit de pagina

---
+++ t4oligo_menu_principal.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>t4oligo</title>
+	<style type="text/css">
+		header{
+			background-color: #4FBDCD;
+			padding: 30px;
+			text-align: center;
+			font-size: 35px;
+			color: white;
+		}
+
+		.cuadrado_principal{
+			padding:5px;
+			margin:5px;
+			width: 200px;
+			height: 200px;
+			background-color: #FCFCD0;
+			border: solid 2px white;
+			color: silver;
+			/*text-align: center;*/
+			display: flex;
+   			justify-content: center;
+   			align-items: center;
+		}
+
+		.cuadros_sintesis{
+			width: 200px;
+			height: 50px;
+			background-color: #B4C6E7;
+			border: solid 2px white;
+			color: black;
+			/*text-align: center;*/
+			display: flex;
+   			justify-content: center;
+   			align-items: center;
+		}
+
+		.cuadros_equipo{
+			width: 200px;
+			height: 50px;
+			background-color: #70AD47;
+			border: solid 2px white;
+			color: black;
+			/*text-align: center;*/
+			display: flex;
+   			justify-content: center;
+   			align-items: center;
+		}
+
+		.cuadros_pre_reactivos{
+			width: 200px;
+			height: 50px;
+			background-color: #FFE699;
+			border: solid 2px white;
+			color: black;
+			text-align: center;
+			display: flex;
+   			justify-content: center;
+   			align-items: center;
+		}
+
+		.cuadros_sintesis_b{
+			width: 200px;
+			height: 50px;
+			background-color: #B4C6E7;
+			border: solid 2px white;
+			color: black;
+			text-align: center;
+			display: flex;
+   			justify-content: center;
+   			align-items: center;
+		}
+
+		.cuadros_equipo_b{
+			width: 200px;
+			height: 50px;
+			background-color: #70AD47;
+			border: solid 2px white;
+			color: black;
+			text-align: center;
+			display: flex;
+   			justify-content: center;
+   			align-items: center;
+		}
+
+		.cuadros_pre_reactivos_b{
+			width: 200px;
+			height: 50px;
+			background-color: #FFE699;
+			border: solid 2px white;
+			color: black;
+			text-align: center;
+			display: flex;
+   			justify-content: center;
+   			align-items: center;
+		}
+	</style>
+</head>
+<body>
+	<header>CURSO DE CAPACITACION T4OLIGO</header>
+
+	<div>
+		<div align="right">
+			<div class="cuadros_sintesis_b">Definicion</div>
+		</div>
+	</div>
+	<div>
+		<div align="right">
+			<div class="cuadros_sintesis_b">Ciclo de sintesis</div>
+		</div>
+
+	</div>
+	<div>
+		<div align="right">
+			<div class="cuadros_sintesis_b">Archivo de sintesis y orden maestra</div>
+		</div>
+	</div>
+	<div>
+		<div>
+			<div class="cuadros_sintesis">SINTESIS
+			<div class="cuadros_sintesis_b">¿Como programar sintesis en ABI 3900?</div>
+		</div>
+	</div>
+	<div>
+		<div align="right">
+			<div class="cuadros_sintesis_b">¿Como programar sintesis en DR OLIGO 96?</div>
+		</div>
+	</div>
+	<div>
+		<div align="right">
+			<div class="cuadros_sintesis_b">¿Como sintetizar oligos modificados, marcados o sondas?</div>
+	</div>
+	<div>
+		<div align="right">
+			<div class="cuadros_sintesis_b">¿Como y cuando remover DMT?</div>
+		</div>
+	</div>
+</body>
+</html>
